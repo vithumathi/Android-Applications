c601f4162b848cbf19e335f25775679eade6a6d9
Vithu Mathi <vithu267@hotmail.com>
2018-12-16 10:02:22 -0500

    Added audio files for each tamil words

--- SpeakTamil/app/src/main/java/com/example/android/SpeakTamil/PhrasesActivity.java
+++ SpeakTamil/app/src/main/java/com/example/android/SpeakTamil/PhrasesActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.SpeakTamil;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,18 +14,29 @@ public class PhrasesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
-        ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("Where are you going?", "Ni enke pokirai?", "நீ எங்கே போகிறாய்?"));
-        words.add(new Word("What is your name?", "Unkal peyar enna?", "உங்கள் பெயர் என்ன?"));
-        words.add(new Word("My name is...", "En peyar...", "என் பெயர்"));
-        words.add(new Word("How are you feeling?", "Ni eppadi irukkirai", "நீ எப்படி இருக்கிறாய்"));
-        words.add(new Word("I’m doing good.", "Nan nanraka irukiren", "நான் நன்றாக இருகிறேன்"));
-        words.add(new Word("Are you coming?", "Ni varukiraya?", "நீ வருகிறாயா?"));
-        words.add(new Word("Yes, I’m coming.", "Am, nan varukiren", "ஆம், நான் வருகிறேன்"));
-        words.add(new Word("Let’s go.", "Nam pokalam", "நாம் போகலாம்"));
-        words.add(new Word("Come here.", "Inke va", "இங்கே வா"));
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("Where are you going?", "Ni enke pokirai?", "நீ எங்கே போகிறாய்?", R.raw.where_are_you_going));
+        words.add(new Word("What is your name?", "Unkal peyar enna?", "உங்கள் பெயர் என்ன?", R.raw.what_is_your_name));
+        words.add(new Word("My name is...", "En peyar...", "என் பெயர்", R.raw.my_name_is));
+        words.add(new Word("How are you?", "Ninkal eppadi irukkirirkal", "நீங்கள் எப்படி இருக்கிறீர்கள்", R.raw.how_are_you));
+        words.add(new Word("I’m doing well.", "Nan nanraka irukiren", "நான் நன்றாக இருகிறேன்", R.raw.im_doing_well));
+        words.add(new Word("Are you coming?", "Ni varukiraya?", "நீ வருகிறாயா?", R.raw.are_you_coming));
+        words.add(new Word("Yes, I’m coming.", "Am, nan varukiren", "ஆம், நான் வருகிறேன்", R.raw.yes_i_am_coming));
+        words.add(new Word("Let’s go.", "lets_go", "போகலாம்", R.raw.lets_go));
+        words.add(new Word("Come here.", "Inke va", "இங்கே வா", R.raw.come_here));
         WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Word word = words.get(position);
+                MediaPlayer mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getmAduioResourceId());
+                mMediaPlayer.start();
+            }
+        });
+
+
     }
 }
--- SpeakTamil/app/src/main/java/com/example/android/SpeakTamil/Word.java
+++ SpeakTamil/app/src/main/java/com/example/android/SpeakTamil/Word.java
@@ -23,7 +23,10 @@ public class Word {
     private int mImageResourceId = NO_IMAGE_PROVIDED;
 
     private static final int NO_IMAGE_PROVIDED = -1;
-
+    /**
+     * Audio resource ID for the word
+     */
+    private int mAduioResourceId;
 
 
     /**
@@ -33,11 +36,13 @@ public class Word {
      * @param defaultTranslation is the word in a language that the user is already familiar with (such as English)
      * @param tamilTranslation is the word in the Tamil language
      * @param tamilWord is the word written in Tamil language
+     * @param aduioResourceId is the resource ID for the audio file associated with this word
      */
-    public Word (String defaultTranslation, String tamilTranslation, String tamilWord){
+    public Word (String defaultTranslation, String tamilTranslation, String tamilWord, int aduioResourceId){
         this.mDefaultTranslation = defaultTranslation;
         this.mTamilTranslation = tamilTranslation;
         this.mTamilWord = tamilWord;
+        this.mAduioResourceId = aduioResourceId;
     }
 
     /**
@@ -48,12 +53,14 @@ public class Word {
      * @param tamilTranslation is the word in the Tamil language
      * @param tamilWord is the word written in Tamil language
      * @param imageId is the drawable resource ID for the image asset
+     * @param aduioResourceId is the resource ID for the audio file associated with this word
      */
-    public Word (String defaultTranslation, String tamilTranslation, String tamilWord, int imageId){
+    public Word (String defaultTranslation, String tamilTranslation, String tamilWord, int imageId, int aduioResourceId){
         this.mDefaultTranslation = defaultTranslation;
         this.mTamilTranslation = tamilTranslation;
         this.mTamilWord = tamilWord;
         this.mImageResourceId = imageId;
+        this.mAduioResourceId = aduioResourceId;
     }
 
     public String getmDefaultTranslation() {
@@ -66,6 +73,10 @@ public class Word {
 
     public String getmTamilWord() {
         return mTamilWord;
+    }
+
+    public int getmAduioResourceId() {
+        return mAduioResourceId;
     }
 
     public int getmImageResourceId() {
