c54ac8647dde98eabbc306654d891b5fa92c9b04
Vithu Mathi <vithu267@hotmail.com>
2018-12-20 09:08:17 -0500

    Added Now playing layout design & action buttons

--- Tones/app/src/main/java/com/mathiap/tones/MyLibraryFragment.java
+++ Tones/app/src/main/java/com/mathiap/tones/MyLibraryFragment.java
@@ -5,6 +5,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +22,33 @@ public class MyLibraryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.song_list, container, false);
+        final ArrayList<Song> songs = new ArrayList<Song>();
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song1", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song2", "Album1", "Artist1", R.drawable.cafe_coffee));
+        songs.add(new Song("Song3", "Album1", "Artist1", R.drawable.cafe_coffee));
+        SongAdapter itemAdapter = new SongAdapter(getActivity(), songs);
+        GridView gridView = (GridView) rootView.findViewById(R.id.list);
+        gridView.setAdapter(itemAdapter);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_library, container, false);
+        return rootView;
     }
 }
