410b6dd45b8cda5ab5ff30f804aec7e1a596726b
Vithu Mathi <vithu267@hotmail.com>
2018-12-26 19:39:36 -0500

    Added loader for async network job

--- MyNews/app/src/main/java/com/mathiap/mynews/MainActivity.java
+++ MyNews/app/src/main/java/com/mathiap/mynews/MainActivity.java
@@ -1,21 +1,44 @@
 package com.mathiap.mynews;
 
-import android.os.AsyncTask;
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import static com.mathiap.mynews.QueryUtils.fetchEarthquakeData;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements LoaderManager.LoaderCallbacks<List<NewsArticle>> {
     /**
      * URL to query the USGS dataset for earthquake information
      */
     private static final String USGS_REQUEST_URL =
             "https://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2014-01-01&api-key=test";
 
+    /**
+     * Constant value for the news article loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<List<NewsArticle>> onCreateLoader(int i, Bundle bundle) {
+        return new NewsArticleLoader(this, USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<NewsArticle>> loader, List<NewsArticle> newsArticles) {
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<NewsArticle>> loader) {
     }
 }
